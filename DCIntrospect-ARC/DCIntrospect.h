8c31b8aa171b070cd93c56e5d3d9330604247632
Steven Hepting <shepting@gmail.com>
2016-02-03 13:53:07 -0800

    Add <UIKit/UIKit.h> import
    For projects that don't have <UIKit/UIKit.h> in their precompiled header, this is required to fix build errors.

--- DCIntrospect-ARC/DCIntrospect.h
+++ DCIntrospect-ARC/DCIntrospect.h
@@ -8,6 +8,7 @@
 #define kDCIntrospectNotificationIntrospectionDidEnd @"kDCIntrospectNotificationIntrospectionDidEnd"
 #define kDCIntrospectAnimationDuration 0.08
 
+#import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 #include "TargetConditionals.h"
 
